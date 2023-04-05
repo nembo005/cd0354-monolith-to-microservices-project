72bbf752f59212883a02dd6a87aeb8874d69dfc2
nembo005 <nembotchedjou35@gmail.com>
2023-04-05 09:16:22 +0100

    initial comit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=nembo
+export POSTGRES_PASSWORD=nemboche1234
+export POSTGRES_HOST=ai.cjsuouiy04wb.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::test-209862822072-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
